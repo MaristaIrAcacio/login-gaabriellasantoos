eb9bf7cb351a73fac227d8da36f46f95ccb3bb34
Gabriella Santos <goncalves.gabriella@alunoescolassociais.org.br>
2023-06-27 11:14:02 -0300

    adiciona os arquivos ao projeto

---
+++ login.js
@@ -0,0 +1,8 @@
+// Bloco de verificação de preenchimento de campos
+
+function Verifica(){
+    let user = document.getElementById('email').value;
+    let senha = document.getAnimations('loginsenha').value;
+    console.log(user);
+    console.log(senha);
+}
