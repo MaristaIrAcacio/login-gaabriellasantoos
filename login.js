8ad9dae3ae52a34fb1042756f1c90029dc103379
Gabriella Santos <goncalves.gabriella@alunoescolassociais.org.br>
2023-07-26 09:41:09 -0300

    criar um alerta digite um nome

--- login.js
+++ login.js
@@ -22,24 +22,35 @@ function salvarUser(){
 
     if(nomeUser){
         dadosLista.push (nomeUser);
-        criaLista()
+        criaLista();
+        document.getElementById("nomeUser").value= '';
+    }else{
+        alert("Digite um nome");
+        document.getElementById("nomeUser").focus();
+    }
+
         
         function criaLista(){
             let tabela = document.getElementById("tabela").innerHTML = "<tr><th>Nome Usuário</th><th>Ações<th></tr>";
             
             for(let i=0; i <= (dadosLista.length-1);i++){
-                tabela += "<tr><td>" + dadosLista[i] + "</td><td><button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button onclick=''>Excluir</button></td></tr>";
+                tabela += "<tr><td>" + dadosLista[i] + "</td><td><button onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
                 document.getElementById('tabela').innerHTML = tabela;
             }
 
 
         }
     }
-}
+
 //FUNÇÃO PARA EDIÇÃO DE NOME
 function editar(i){
     document.getElementById("nomeUser").value = dadosLista[(i - 1)];
-    dadosLista.splice(dadosLista[(i-1), 1]);
+    dadosLista.splice(dadosLista[(i-1)], 1);
 
 }
 //FUNÇÃO PARA EXCLUIR O NOME
+
+function excluir(i){
+    dadosLista.splice((i - 1), 1);
+    document.getElementById("tabela").deleteRow(i);
+}
